136856725bb899c2d3c1d3375d54857b5c51c15d
HannahFabian <0252139@up.edu.mx>
2024-10-27 16:51:26 -0600

    Update index.js

--- Portfolio/09_ExpressPersonalAPI/index.js
+++ Portfolio/09_ExpressPersonalAPI/index.js
@@ -5,14 +5,12 @@ const path = require('path');
 const names = []; 
 let tasks = [];
 
-// Configurar la carpeta pública para servir archivos estáticos
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/html/index.html');
 });
 
-// Ruta para manejar el formulario de saludo y redirigir de vuelta a la raíz
 app.post('/greet', (req, res) => {
     const name = req.body.name; 
     if (name) {
@@ -23,22 +21,21 @@ app.post('/greet', (req, res) => {
 
 app.route('/task')
     .post((req, res) => {
-        const task = req.body.task; // Obtener la tarea del formulario
+        const task = req.body.task; 
         if (task) {
-            tasks.push(task); // Agregar tarea
+            tasks.push(task); 
         }
-        res.redirect('/'); // Redirigir a la página principal
+        res.redirect('/'); 
     });
 
 app.post('/delete-task', (req, res) => {
-      const index = parseInt(req.body.index); // Obtener el índice de la tarea a eliminar
+      const index = parseInt(req.body.index); 
       if (!isNaN(index)) {
-          tasks.splice(index, 1); // Eliminar tarea del array
+          tasks.splice(index, 1); 
       }
-      res.redirect('/'); // Redirigir a la página principal
+      res.redirect('/'); 
   });
 
-// Iniciar el servidor
 app.listen(8000, () => {
   console.log('Servidor escuchando en el puerto 8000');
 });
